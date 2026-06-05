121962d10fe23a0c50834a02ebacf90cc63013e8
sagargusain777 <sagargusain201012@gmail.com>
2026-06-05 17:47:38 +0000

    Creation Logic Validation Added fo Ticket

--- srv/customerportal-service.js
+++ srv/customerportal-service.js
@@ -1,6 +1,8 @@
-
+'use strict';
 
 const cds = require('@sap/cds');
+
+
 
 module.exports = class CustomerPortalService extends cds.ApplicationService {
 
@@ -14,10 +16,44 @@ module.exports = class CustomerPortalService extends cds.ApplicationService {
 
 
 
+        this.before('CREATE', Customers, async (req) => {
+
+            const resultNumber = await SELECT.one`count(*) as cnt`.from(Customers);
+
+            let currentCount = 0;
+
+            if (resultNumber && resultNumber.cnt) {
+                currentCount = parseInt(resultNumber.cnt);
+            }
+
+            let nextCustomerNumber = currentCount + 1;
+            let paddedCustomerNumber = nextCustomerNumber.toString().padStart(6, '0');
+            req.data.customerNumber = `CUST-${paddedCustomerNumber}`;
+            req.data.isActive = true;
+        }
+        )
+
+        this.before('CREATE', ServiceTickets, async (req) => {
+            const resultNumber = await SELECT.one`count(*)as cnt`.from(ServiceTickets);
+            let currentCount = 0;
+            if (resultNumber && resultNumber.cnt) {
+                currentCount = parseInt(resultNumber.cnt);
+            }
+
+            let nextTicketNumber = currentCount + 1;
+            let paddedTicketNumber = nextTicketNumber.toString().padStart(6, '0');
+            req.data.ticketNumber = `TKT-${paddedTicketNumber}`;
+            if (!req.data.status_code) req.data.status_code = 'NEW';
+            if (!req.data.priority_code) req.data.priority_code = 'MED';
+        })
+
+
 
         // Always call super.init() at the end
         await super.init()
     }
+
+
 
 
 }
