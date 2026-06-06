bb0c9536b42078153dd289ff6c05798cb5169db2
sagargusain777 <sagargusain201012@gmail.com>
2026-06-06 06:50:10 +0000

    Added Custom Logic for Opportunities

--- srv/customerportal-service.js
+++ srv/customerportal-service.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const cds = require('@sap/cds');
+const { SELECT } = require('@sap/cds/lib/ql/cds-ql');
 
 
 
@@ -46,6 +47,27 @@ module.exports = class CustomerPortalService extends cds.ApplicationService {
             if (!req.data.status_code) req.data.status_code = 'NEW';
             if (!req.data.priority_code) req.data.priority_code = 'MED';
         })
+
+
+
+        this.before('CREATE',Opportunities,async(req)=>{
+
+            const resultNumber = await SELECT.one `count(*) as cnt`.from(Opportunities);
+            let currentCount = 0;
+            if (resultNumber && resultNumber.cnt) {
+                currentCount = parseInt(resultNumber.cnt);
+            }
+
+            let nextOpportunityNumber = currentCount + 1;
+            let paddedOpportunityNumber = nextOpportunityNumber.toString().padStart(6, '0');
+            req.data.opportunityNumber = `OPP-${paddedOpportunityNumber}`;
+
+            if( !req.data.stage_code) req.data.stage_code = 'PROSPECT';
+
+        }
+
+        
+    )
 
 
 
