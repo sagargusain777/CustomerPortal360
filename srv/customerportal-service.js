9d680b690c1b6be3aa9125ead0172ff731f35019
sagargusain777 <sagargusain201012@gmail.com>
2026-06-06 09:28:11 +0000

    Added Reolution action in the Service Tickets

--- srv/customerportal-service.js
+++ srv/customerportal-service.js
@@ -64,39 +64,57 @@ module.exports = class CustomerPortalService extends cds.ApplicationService {
 
             if (!req.data.stage_code) req.data.stage_code = 'PROSPECT';
 
-        } )
-
-     // Befor create and Update : Contact Full Name 
-     this.before(['CREATE', 'UPDATE'], Contacts , async (req) =>  {
-        const { firstName, lastName} = req.data;
-        if(!firstName || firstName === undefined){
-            firstName = ' ';
-        }
-
-        if(!lastName || lastName === undefined){
-            lastName = ' ';
-        }
-
-        req.data.fullName = `${firstName} ' ' ${lastName}`.trim();
-
-     })
-
-     // Action Escalate
-     this.on('escalate',ServiceTickets, async (req) => {
-        const {ID} = req.params[0];
-        const {reason} = req.data;
-
-        await UPDATE(ServiceTickets).set({status_code: 'ESCALATED'}).where({ID: ID});
-
-        await INSERT.into(TicketComments).entries({
-            ticket_ID : ID,
-            comment: `Ticket escalated. Reason: ${reason}`,
-            isInternal: true,
-            author: req.user?.id || 'system'
         })
 
-          return SELECT.one(ServiceTickets).where({ ID });
-     })
+        // Befor create and Update : Contact Full Name 
+        this.before(['CREATE', 'UPDATE'], Contacts, async (req) => {
+            const { firstName, lastName } = req.data;
+            if (!firstName || firstName === undefined) {
+                firstName = ' ';
+            }
+
+            if (!lastName || lastName === undefined) {
+                lastName = ' ';
+            }
+
+            req.data.fullName = `${firstName} ' ' ${lastName}`.trim();
+
+        })
+
+        // Action Escalate
+        this.on('escalate', ServiceTickets, async (req) => {
+            const { ID } = req.params[0];
+            const { reason } = req.data;
+
+            await UPDATE(ServiceTickets).set({ status_code: 'ESCALATED' }).where({ ID: ID });
+
+            await INSERT.into(TicketComments).entries({
+                ticket_ID: ID,
+                comment: `Ticket escalated. Reason: ${reason}`,
+                isInternal: true,
+                author: req.user?.id || 'system'
+            })
+
+            return SELECT.one(ServiceTickets).where({ ID });
+        })
+        // Action Resolve
+        this.on('resolve', ServiceTickets, async (req) => {
+
+            const { ID } = req.params[0];
+            const { resolution } = req.data;
+
+
+            await UPDATE(ServiceTickets).set({
+                status_code: 'RESOLVED',
+                resolution: resolution,
+                resolvedAt: new Date().toISOString()
+
+            }).where({ ID: ID });
+
+
+            return SELECT.one(ServiceTickets).where({ ID });
+
+        })
 
         // Always call super.init() at the end
         await super.init()
